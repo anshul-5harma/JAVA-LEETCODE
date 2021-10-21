a20393b859979778bf8f499656f5fa556d3876a3
Anshul Sharma <anshulsharma427@gmail.com>
2021-10-21 00:31:26 -0400

    Time: 1 ms (70.35%), Space: 39 MB (16.32%) - LeetHub

---
+++ goal-parser-interpretation/goal-parser-interpretation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < command.length(); i++) {
+            if(command.charAt(i) == '(' && command.charAt(i+1) == ')') {
+                sb.append("o");
+                ++i;
+            } else if(command.charAt(i) == '(' || command.charAt(i) == ')') {
+                sb.append("");
+            } else {
+                sb.append(command.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
