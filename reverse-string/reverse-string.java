e8ecc95591e5cc1daf527e66455b4a4ef8951497
Anshul Sharma <anshulsharma427@gmail.com>
2021-10-25 23:58:18 -0400

    Time: 2 ms (29.88%), Space: 52.5 MB (36.95%) - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
         int low = 0;
-        int high = s.length - 1 % 2;
+        int high = s.length - 1;
         while(low < high) {
             char ch = s[low];
             s[low] = s[high];
