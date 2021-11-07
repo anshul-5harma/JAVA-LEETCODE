4010e96ce0fd4388903930eaaa840fce8ed1da09
Anshul Sharma <anshulsharma427@gmail.com>
2021-11-07 14:34:46 -0500

    Time: 0 ms (100.00%), Space: 37.1 MB (68.94%) - LeetHub

--- maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -1,6 +1,5 @@
 class Solution {
     public int maxDepth(String s) {
-        s = s.replaceAll("[^()]", "");
         int counter = 0;
         int MAX = Integer.MIN_VALUE;
         if(s.length() == 0) {
@@ -9,7 +8,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '(') {
                 counter++;
-            } else {
+            } else  if(s.charAt(i) == ')') {
                 counter--;
             }
             MAX = Math.max(MAX, counter);
