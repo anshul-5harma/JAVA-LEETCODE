41b28bed23936e7385d6e42a1b6de32212cfd3e7
Anshul Sharma <anshulsharma427@gmail.com>
2021-11-10 22:16:12 -0500

    Time: 1 ms (60.71%), Space: 39.3 MB (67.68%) - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -1,15 +1,26 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int max = nums.length;
-        Set<Integer> set = new HashSet();
-        for(int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+    public int missingNumber(int[] arr) {
+        int i = 0;
+        while(i < arr.length) {
+            int correct = arr[i];
+            if(arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
         }
-        for(int j = 0; j <= max; j++) {
-            if(!set.contains(j)) {
+        for(int j = 0; j < arr.length; j++) {
+            if(arr[j] != j) {
                 return j;
             }
         }
-        return 0;
+        return arr.length;
+    }
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
+
+// cyclic sort variation Amazon Question Credits - Kunal Khuswaha
