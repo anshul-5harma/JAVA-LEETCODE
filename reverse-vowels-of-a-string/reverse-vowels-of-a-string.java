d7277ea8a4b7f1066b33349ca773c23fd122e07e
Anshul Sharma <anshulsharma427@gmail.com>
2021-11-11 16:18:56 -0500

    Time: 4 ms (70.92%), Space: 39.1 MB (76.99%) - LeetHub

---
+++ reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char[] ch = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U') {
+                stack.push(ch[i]);
+            }
+        }
+
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U') {
+                //stack.push(ch[i]);
+                ch[i] = stack.pop();
+            }
+        }
+        String final_str = new String(ch);
+        return final_str;
+    }
+}
