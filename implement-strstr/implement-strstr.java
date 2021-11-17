17cc3aed6d2661e3d749f584824fce8e2cfa394c
Anshul Sharma <anshulsharma427@gmail.com>
2021-11-17 00:58:54 -0500

    Time: 324 ms (67.17%), Space: 38.9 MB (86.30%) - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,13 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int index = -1;
-        if(needle.equals("")) {
-            index = 0;
-        } else if(haystack.length() < needle.length()) {
-            index = -1;
-        } else {
-            index = haystack.indexOf(needle);  
-        }
-        return index;
+        return haystack.indexOf(needle);
     }
 }
