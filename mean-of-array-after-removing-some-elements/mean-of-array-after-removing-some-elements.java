2d9c792eada27a0ee61a8ffee142cda1fa545ee5
Anshul Sharma <anshulsharma427@gmail.com>
2021-12-17 00:49:24 -0500

    Time: 4 ms (35.34%), Space: 41.5 MB (13.10%) - LeetHub

---
+++ mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.java
@@ -0,0 +1,23 @@
+// class Solution {
+//     public double trimMean(int[] arr) {
+//         Arrays.sort(arr);
+//         int remove = (int) (0.05 * arr.length);
+//         int sum = 0;
+//         for(int i = remove; i < arr.length - remove; i++) {
+//             sum += arr[i];
+//         }
+//         return sum / (arr.length - (2 * remove));
+//     }
+// }
+
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int remove = (int)(0.05 * (arr.length));
+        double sum = 0.0;
+        for(int i = remove; i < arr.length-remove; i++){
+            sum += arr[i];
+        }
+        return sum/(arr.length-(2*remove));
+    }
+}
