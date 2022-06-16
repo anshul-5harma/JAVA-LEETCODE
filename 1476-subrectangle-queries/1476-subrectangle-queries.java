fc603f846b25cab3f13415aa8d8b256fe26cace7
Anshul Sharma <anshulsharma427@gmail.com>
2022-06-15 23:21:33 -0400

    Time: 26 ms (99.41%), Space: 44.7 MB (94.90%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.java
+++ 1476-subrectangle-queries/1476-subrectangle-queries.java
@@ -1,5 +1,5 @@
 class SubrectangleQueries {
-    private int[][] rectangle;
+    int[][] rectangle;
     public SubrectangleQueries(int[][] rectangle) {
         this.rectangle = rectangle;
     }
@@ -7,7 +7,7 @@ class SubrectangleQueries {
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
         for(int i = row1; i <= row2; i++) {
             for(int j = col1; j <= col2; j++) {
-                this.rectangle[i][j] = newValue;
+                rectangle[i][j] = newValue;
             }
         }
     }
